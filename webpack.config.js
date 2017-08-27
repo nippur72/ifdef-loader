ffaa076ff8bd7a6569b0adcc83a4040c91c5fe17
nino-porcino <nino.porcinoREMOVETHIS@gmail.com>
2017-08-27 18:53:22 +0200

    test with real webpack environment

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/*
+ * This file is used to test ifdef-loader on a real webpack environment.
+ * It reads "spec/data/webpack.in.js" and outputs "spec/data/webpack.out.actual.js" 
+ * which is later compared with "spec/data/webpack.out.js"
+ * 
+ */
+
+const opts = {
+   DEBUG: true,
+   version: 3,
+   "ifdef-verbose": true,        // add this for verbose output
+   "ifdef-triple-slash": true    // add this to use double slash comment instead of default triple slash
+};
+
+// if using query strings
+const q = require('querystring').encode(opts);
+
+/************************************************************/
+
+const webpack = require('webpack');
+
+module.exports = function(env)
+{   
+   const entry = "./spec/data/webpack.in.js";
+
+   const loaders = [{ 
+      test: /\.in\.js$/, 
+      exclude: /node_modules/, 
+      use: [{ 
+         loader: `${__dirname}/ifdef-loader`,
+         options: opts 
+      }]
+      // alternate: 
+      // loader: `${__dirname}/ifdef-loader?query=${q}`,
+   }];
+      
+   let config =
+   {
+       module: { rules: loaders },
+       entry: entry,
+       cache: true,
+
+       output: {
+           libraryTarget: "this",
+           path: `${__dirname}/spec/data`,
+           filename: "webpack.out.actual.js",
+       }       
+   };
+
+   return config;
+};
+
