9cb6ec9e5997d060699b49540d543780bbba6a48
GWigWam <welwisam@gmail.com>
2019-01-09 16:24:57 +0100

    Rewrite to parse nested ifs recursively
    This will prevent lines from being Regexed multiple times. This improves readability and may improve performance.

--- preprocessor.ts
+++ preprocessor.ts
@@ -1,7 +1,12 @@
-interface IStart {
-   line: number;
-   keyword: string;
-   condition: string;
+/** Holds the line indexes for a complete #if block */
+class IfBlock {
+   /**
+    * @param startIx Line index of #if
+    * @param endIx Line index of #endif
+    * @param elseIx Line index of #else, or null
+    * @param innerIfs List of any IfBlocks that are contained within this IfBlock
+    */
+   constructor(public startIx: number, public endIx: number, public elseIx: number|null = null, public innerIfs: IfBlock[] = []) { }
 }
 
 let useTripleSlash: boolean|undefined;
@@ -12,58 +17,85 @@ export function parse(source: string, defs: object, verbose?: boolean, tripleSla
 
    const lines = source.split('\n');
 
-   for(let n=0;;) {
-      let startInfo = find_start_if(lines,n);
-      if(startInfo === undefined) break;
-
-      const endLine = find_end(lines, startInfo.line);
-      if(endLine === -1) {
-         throw `#if without #endif in line ${startInfo.line+1}`;
-      }
-
-      const elseLine = find_else(lines, startInfo.line, endLine);
-
-      const cond = evaluate(startInfo.condition, startInfo.keyword, defs);
-
-      if(cond) {
-         if(verbose) {
-            console.log(`matched condition #${startInfo.keyword} ${startInfo.condition} => including lines [${startInfo.line+1}-${endLine+1}]`);
-         }
-         blank_code(lines, startInfo.line, startInfo.line);
-         if (elseLine === -1) {
-            blank_code(lines, endLine, endLine);
-         } else {
-            blank_code(lines, elseLine, endLine);
-         }
-      } else {
-         if (elseLine === -1) {
-            blank_code(lines, startInfo.line, endLine);
-         } else {
-            blank_code(lines, startInfo.line, elseLine);
-            blank_code(lines, endLine, endLine);
-         }
-         if(verbose) {
-            console.log(`not matched condition #${startInfo.keyword} ${startInfo.condition} => excluding lines [${startInfo.line+1}-${endLine+1}]`);
-         }
-      }
-
-      n = startInfo.line;
+   var ifBlocks = find_if_blocks(lines);
+   for(let ifBlock of ifBlocks) {
+      apply_if(lines, ifBlock, defs, verbose);
    }
 
    return lines.join('\n');
 }
 
-function match_if(line: string): IStart|undefined {
-   const re = useTripleSlash ? /^[\s]*\/\/\/([\s]*)#(if)([\s\S]+)$/g : /^[\s]*\/\/([\s]*)#(if)([\s\S]+)$/g;
+function find_if_blocks(lines: string[]): IfBlock[] {
+   const blocks: IfBlock[] = [];
+   for(let i = 0; i < lines.length; i++) {
+      if(match_if(lines[i])) {
+         const ifBlock = parse_if_block(lines, i);
+         blocks.push(ifBlock);
+         i = ifBlock.endIx;
+      }
+   }
+   return blocks;
+}
+
+/**
+ * Parse #if statement at given locatoin
+ * @param ifBlockStart Line on which the '#if' is located. (Given line MUST be start of an if-block)
+ */
+function parse_if_block(lines: string[], ifBlockStart: number): IfBlock {
+   let foundElse: number|null = null;
+   let foundEnd: number|undefined;
+   let innerIfs: IfBlock[] = [];
+
+   for(let i = ifBlockStart + 1; i < lines.length; i++) {
+      const curLine = lines[i];
+
+      const innerIfMatch = match_if(curLine);
+      if(innerIfMatch) {
+         const innerIf = parse_if_block(lines, i);
+         innerIfs.push(innerIf);
+         i = innerIf.endIx;
+         continue;
+      }
+
+      const elseMatch = match_else(curLine);
+      if(elseMatch) {
+         foundElse = i;
+         continue;
+      }
+
+      const endMatch = match_endif(curLine);
+      if(endMatch) {
+         foundEnd = i;
+         break;
+      }
+   }
+
+   if(foundEnd === undefined) {
+      throw `#if without #endif on line ${ifBlockStart + 1}`;
+   }
+   return new IfBlock(ifBlockStart, foundEnd, foundElse, innerIfs);
+}
+
+const ifRegex = () => useTripleSlash ? /^[\s]*\/\/\/([\s]*)#(if)([\s\S]+)$/g : /^[\s]*\/\/([\s]*)#(if)([\s\S]+)$/g;
+
+function match_if(line: string) : boolean {
+   const re = ifRegex();
+   const match = re.exec(line);
+   return Boolean(match);
+}
+
+/**
+ * @param line Line to parse, must be a valid #if statement
+ * @returns The if condition
+ */
+function parse_if(line: string): string {
+   const re = ifRegex();
    const match = re.exec(line);
    if(match) {
-      return {
-         line: -1,
-         keyword: match[2],
-         condition: match[3].trim()
-      };
+      return match[3].trim();
+   } else {
+      throw `Could not parse #if: '${line}'`;
    }
-   return undefined;
 }
 
 function match_endif(line: string): boolean {
@@ -78,65 +110,44 @@ function match_else(line: string): boolean {
    return Boolean(match);
 }
 
-function find_start_if(lines: string[], n: number): IStart|undefined {
-   for(let t=n; t<lines.length; t++) {
-      const match = match_if(lines[t]);
-      if(match !== undefined) {
-         match.line = t;
-         return match;
-         // TODO: when es7 write as: return { line: t, ...match };
-      }
-   }
-   return undefined;
-}
+/** Includes and excludes relevant lines based on evaluation of the provided IfBlock */
+function apply_if(lines: string[], ifBlock: IfBlock, defs: object, verbose: boolean = false) {
+   const ifCond = parse_if(lines[ifBlock.startIx]);
+   const evalRes = evaluate(ifCond, defs);
 
-function find_end(lines: string[], start: number): number {
-   let level = 1;
-   for(let t=start+1; t<lines.length; t++) {
-      const mif  = match_if(lines[t]);
-      const mend = match_endif(lines[t]);
+   const incStart = evalRes ? ifBlock.startIx : ifBlock.elseIx;   // Start of included lines
+   const incEnd = ifBlock.elseIx === null ?                       // End of included lines
+      evalRes ? ifBlock.endIx : null :
+      evalRes ? ifBlock.elseIx : ifBlock.endIx;
+   const excStart = evalRes ? ifBlock.elseIx : ifBlock.startIx;   // Start of excluded lines
+   const excEnd = ifBlock.elseIx === null ?                       // End of excluded lines
+      evalRes ? null : ifBlock.endIx :
+      evalRes ? ifBlock.endIx : ifBlock.elseIx;
 
-      if(mif) {
-         level++;
-      }
-
-      if(mend) {
-         level--;
-         if(level === 0) {
-            return t;
-         }
-      }
-   }
-   return -1;
-}
-
-function find_else(lines: string[], start: number, end: number): number {
-   let level = 1;
-   for(let t=start+1; t<end; t++) {
-      const mif  = match_if(lines[t]);
-      const melse = match_else(lines[t]);
-      const mend = match_endif(lines[t]);
-      if(mif) {
-         level++;
-      }
-
-      if(mend) {
-         level--;
-      }
-
-      if (melse && level === 1) {
-         return t;
-      }
+   if(verbose) {
+      console.log(`Condition '${ifCond}' is ${evalRes ? 'TRUE' : 'FALSE'}.${incStart != null && incEnd != null ? ` including lines [${incStart + 1}-${incEnd + 1}]` : ''}${excStart != null && excEnd != null ? ` excluding lines [${excStart + 1}-${excEnd + 1}]` : ''}`);
    }
 
-   return -1;
+   blank_code(lines, ifBlock.startIx);                      // Erase /// #if line
+   if(ifBlock.elseIx) { blank_code(lines, ifBlock.elseIx) } // Erase /// #else line
+   blank_code(lines, ifBlock.endIx);                        // Erase /// #endif line
+
+   if(excStart !== null && excEnd !== null) {
+      blank_code(lines ,excStart + 1, excEnd - 1);          // Erase body
+   }
+
+   for(let innerIf of ifBlock.innerIfs) {
+      // Apply inner-if blocks only when they are not already erased
+      if((excStart === null || excEnd === null) || innerIf.startIx < excStart || innerIf.startIx > excEnd) {
+         apply_if(lines, innerIf, defs, verbose);
+      }
+   }
 }
 
 /**
  * @return true if block has to be preserved
  */
-function evaluate(condition: string, keyword: string, defs: object): boolean {
-
+function evaluate(condition: string, defs: object): boolean {
    const code = `return (${condition}) ? true : false;`;
    const args = Object.keys(defs);
 
@@ -150,14 +161,10 @@ function evaluate(condition: string, keyword: string, defs: object): boolean {
       throw `error evaluation #if condition(${condition}): ${error}`;
    }
 
-   if(keyword === "ifndef") {
-      result = !result;
-   }
-
    return result;
 }
 
-function blank_code(lines: string[], start: number, end: number) {
+function blank_code(lines: string[], start: number, end: number = start) {
    for(let t=start; t<=end; t++) {
       const len = lines[t].length;
       const lastChar = lines[t].charAt(len-1);
